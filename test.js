cc0f84936551e1958c974145b121709ef14329b9
aburage <aburage.mk.fms@gmail.com>
2019-05-29 13:16:43 +0900

    Feat: add delete function

--- test.js
+++ test.js
@@ -1,23 +1,40 @@
-const list = document.querySelector("ul,#myList");
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
+//  check todo
+const list = document.getElementById('todo');
+list.addEventListener('click', function(event) {
+    if (event.tagName === 'P') {
+        event.classList.toggle('checked');
     }
 }, false);
 
+//  add todo
 function newElement() {
     const newLi = document.createElement("li");
     const inputValue = document.getElementById("myInput").value;
     const text = document.createTextNode(inputValue);
-    if (inputValue != '') {
-        newLi.appendChild(text);
-        document.getElementById("myList").appendChild(newLi);
-        document.getElementById("myInput").value = "";
 
+    if (inputValue != '') {
+        document.getElementById("myList").appendChild(newLi);
+        newLi.id = "todo";
+        const newP = document.createElement("p");
+        newP.id = "name";
         const span = document.createElement("SPAN");
         span.className = "closeButton";
+        newLi.appendChild(newP);
+        newP.appendChild(text);
+        document.getElementById("myInput").value = "";
+
         const txt = document.createTextNode("\u00D7");
         span.appendChild(txt);
         newLi.appendChild(span);
+    }
+}
+
+//  delete todo
+const close = document.getElementById('todo');
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        const lists = document.getElementById('todo');
+        const del = lists[i];
+        lists.parentNode.removeChild(lists[i]);
     }
 }
